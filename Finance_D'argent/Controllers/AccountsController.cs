d5d24d36b5233bd47887fc1eb90ffe7899fb9cf4
patrick <adjegba3414@gmail.com>
2022-02-19 09:54:15 -0500

    Adding Account table

---
+++ Finance_D'argent/Controllers/AccountsController.cs
@@ -0,0 +1,59 @@
+﻿using Finance_D_argent.Data;
+using Microsoft.AspNetCore.Mvc;
+using Finance_D_argent.Models.
+
+namespace Finance_D_argent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult GetAccounts()
+        {
+            try
+            {
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault()+ "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+                var accountData = (from tempcustomer in _db.Accounts select tempcustomer);
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                {
+                    accountData = accountData.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    accountData = accountData.Where(m => m.AccountName.Contains(searchValue)
+                                                || m.Category.Contains(searchValue)
+                                                || m.SubCategory.Contains(searchValue)
+                                                || m.UserName.Contains(searchValue));
+                }
+                recordsTotal = accountData.Count();
+                var data = accountData.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new
+                {
+                    draw = draw,
+                    recordsFiltered = recordsTotal,
+                    recordsTotal = recordsTotal,
+                    data = data
+                };
+                return Ok(jsonData);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Finance_D'argent/Models/AccountsviewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Finance_D_argent.Models
+{
+    public class AccountsviewModel
+    {
+        [Required]
+        [Display(Name ="Account Name")]
+        public string? AcccountName { get; set; } 
+        public string? NormalSide { get; set; }
+
+        [Key]
+        [Display(Name ="Account Number")]
+        public int? AccountNumber { get; set; }
+
+        public string Description { get; set; } 
+
+        public DateTime DateCreatedOn { get; set; }
+        
+        public int Username { get; set; }  
+
+        [DataType(DataType.Currency)]
+        public double? InitialBalance { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Debit { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Credit { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem> Accounts { get; set; }
+
+        [Required]
+        public int Order { get; set; }
+        public string Statement { get; set; }
+        public string Comments { get; set; }
+        public bool Active { get; set; }
+        public bool Contra { get; set; }
+
+    }
+}
